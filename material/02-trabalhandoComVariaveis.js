ad9c719734cddbd0fdf8883c30a968574aad4d49
Breno <mrodrigues.breno@gmail.com>
2020-12-25 11:51:15 -0300

    Pasta material convertida em iniciante para separação dos cursos

--- material/02-trabalhandoComVariaveis.js
+++
@@ -1,8 +0,0 @@
-console.log("Trabahlando com variáveis");
-// Não esqueça dos comentários!
-/* para blocos com multiplas linhas */
-
-const idade = 25;
-const nome = "Bernardo";
-
-console.log(nome + " tem " + idade + " anos.");
--- material/03-operacoesAritmeticas.js
+++
@@ -1,5 +0,0 @@
-console.log("Operações Aritméticas");
-
-console.log(2+2);
-console.log(10+8*2);
-console.log((10+8)*2);
